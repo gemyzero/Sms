dc2ac9f42f9e9e6c9e5e6d15a2edf4260fb7ac97
gemyzero <mohamed0110gemy@gmail.com>
2025-05-09 16:19:06 +0300

    sec commit

--- src/Componets/benfit/Benfits.jsx
+++ src/Componets/benfit/Benfits.jsx
@@ -6,43 +6,43 @@ export default function Benfits() {
   <div className=" " style={{backgroundColor:'#f2f1e5'}}>
       <div className='container p-5'>
         <h1 className='text-center fw-bold' style={{fontSize:'50px'}}>الفوائد</h1>
-      <div className="row text-center">
-        <div className="col-lg-4 my-5">
+      <div className="row text-center justify-content-center">
+        <div className="col-lg-4 col-md-6 col-12 my-5">
           <div className='text-center'>
           <h1 className='mb-4'><FaCommentSms /></h1>
             <h6>خدمة الرسائل المتعددة الوسائط MMS Bulk          </h6>
             <p>مع برنامج Smseg يمكنك إرسال رسالة وسائط متعددة تحتوي على جميع أنواع ملفات الوسائط المتعددة (صورة - صوت...</p>
           </div>
         </div>
-        <div className="col-lg-4 my-5">
+        <div className="col-lg-4 col-md-6 col-12 my-5">
           <div className='text-center'>
           <h1 className='mb-4'><FaCommentSms /></h1>
             <h6>خدمة الرسائل المتعددة الوسائط MMS Bulk          </h6>
             <p>مع برنامج Smseg يمكنك إرسال رسالة وسائط متعددة تحتوي على جميع أنواع ملفات الوسائط المتعددة (صورة - صوت...</p>
           </div>
         </div>
-        <div className="col-lg-4 my-5">
+        <div className="col-lg-4 col-md-6 col-12 my-5">
           <div className='text-center'>
           <h1 className='mb-4'><FaCommentSms /></h1>
             <h6>خدمة الرسائل المتعددة الوسائط MMS Bulk          </h6>
             <p>مع برنامج Smseg يمكنك إرسال رسالة وسائط متعددة تحتوي على جميع أنواع ملفات الوسائط المتعددة (صورة - صوت...</p>
           </div>
         </div>
-        <div className="col-lg-4 my-5">
+        <div className="col-lg-4 col-md-6 col-12 my-5">
           <div className='text-center'>
           <h1 className='mb-4'><FaCommentSms /></h1>
             <h6>خدمة الرسائل المتعددة الوسائط MMS Bulk          </h6>
             <p>مع برنامج Smseg يمكنك إرسال رسالة وسائط متعددة تحتوي على جميع أنواع ملفات الوسائط المتعددة (صورة - صوت...</p>
           </div>
         </div>
-        <div className="col-lg-4 my-5">
+        <div className="col-lg-4 col-md-6 col-12 my-5">
           <div className='text-center'>
           <h1 className='mb-4'><FaCommentSms /></h1>
             <h6>خدمة الرسائل المتعددة الوسائط MMS Bulk          </h6>
             <p>مع برنامج Smseg يمكنك إرسال رسالة وسائط متعددة تحتوي على جميع أنواع ملفات الوسائط المتعددة (صورة - صوت...</p>
           </div>
         </div>
-        <div className="col-lg-4 my-5">
+        <div className="col-lg-4 col-md-6 col-12 my-5">
           <div className='text-center'>
           <h1 className='mb-4'><FaCommentSms /></h1>
             <h6>خدمة الرسائل المتعددة الوسائط MMS Bulk          </h6>
